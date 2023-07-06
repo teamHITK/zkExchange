6f165b284e2aff7bf46996056233b44672f06acd
agnxsh <iamagnix@gmail.com>
2023-07-06 13:05:40 +0530

    feat: working on circom tester

--- zkCircuits/test/circuit.js
+++ zkCircuits/test/circuit.js
@@ -3,23 +3,28 @@ const { assert } = require("chai");
 const wasm_tester = require("circom_tester").wasm;
 const F1Field = require("ffjavascript").F1Field;
 const Scalar = require("ffjavascript").Scalar;
+
 exports.p = Scalar.fromString(
   "21888242871839275222246405745257275088548364400416034343698204186575808495617"
 );
 const Fr = new F1Field(exports.p);
-const { MerkleSumTree } = require("pyt-merkle-sum-tree");
+
+const { MerkleSumTree } = require("0xagnish/zk-data-prep");
+
 const createCircomInput = require("./helperfunc.js");
 
 describe("Circuit Testing", function async() {
   const pathToCsv = "test/entryPatterns/entry-16.csv";
   // total sum of the liablities is 3273939304
-  console.log("creating merkle tree...");
-  const tree = new MerkleSumTree(pathToCsv); // Init a tree from the entries in the csv file
+  console.log("Constructing Merkle Sum Tree");
+  console.log("Initializing the Merkle Sum Tree from the given data set......");
+  const tree = new MerkleSumTree(pathToCsv);
 
-  // get the index of the user I want to create a proof for
+  //getting the index of the user we want to create a proof for
+  console.log("Getting the Merkle Proof for that particular user.....");
   const entryIndex = tree.indexOf("dxGaEAii", BigInt(11888));
 
-  // create merkle tree proof for that user
+  //creating a Merkle Proof for that user
   const proof = tree.createProof(entryIndex);
 
   beforeEach(async function () {
@@ -29,123 +34,129 @@ describe("Circuit Testing", function async() {
     );
   });
 
-  it("should verify a proof of inclusion of an existing entry if assetsSum > liabilitiesSum", async () => {
-    // pack into circom input adding assetsSum property - assetsSum > liabilitiesSum
+  it("Should verify a proof of inclusion of an existing entry if assetsSum > liabilitiesSum", async () => {
+    console.log(
+      "Packing the inputs into the circom zkSNARK construction..., putting assets 1 more than liabilities..."
+    );
     const input = createCircomInput(proof, BigInt(3273939305));
-
+    console.log("Computing the leaf hashes of the given user data");
     const expectedLeafHashOutput = proof.entry.computeLeaf().hash;
-
-    // // Calculate the witness
+    console.log("Calculating the witness....");
     let witness = await circuit.calculateWitness(input);
     await circuit.assertOut(witness, { leafHash: expectedLeafHashOutput });
     await circuit.checkConstraints(witness);
+    console.log("Proof Verified!");
   });
 
-  it("should verify a proof of inclusion of an existing entry if assetsSum  = liabilitiesSum", async () => {
-    // pack into circom input adding assetsSum property - assetsSum = liabilitiesSum
+  it("Should verify a proof of inclusion of an existing entry if assetsSum = liabilitiesSum", async () => {
+    console.log("Packing the inputs into the circom zkSNARK construction...");
     const input = createCircomInput(proof, BigInt(3273939304));
-
+    console.log("Computing the leaf hashes of the given user data");
     const expectedLeafHashOutput = proof.entry.computeLeaf().hash;
-
-    // Calculate the witness
+    console.log("Calculating the witness....");
     let witness = await circuit.calculateWitness(input);
     await circuit.assertOut(witness, { leafHash: expectedLeafHashOutput });
     await circuit.checkConstraints(witness);
+    console.log("Proof Verified!");
   });
 
-  it("shouldn't verify a proof of inclusion of an existing entry if assetsSum < liabilitiesSum", async () => {
-    // pack into circom input adding assetsSum property - assetsSum < liabilitiesSum
+  it("Should NOT verify a proof of inclusion of an existing entry if assetsSum < liabilitiesSum", async () => {
+    console.log("Packing the inputs into the circom zkSNARK construction...");
     const input = createCircomInput(proof, BigInt(3273939303));
-
     try {
+      console.log("Computing the leaf hashes of the given user data");
+      console.log("Calculating the witness....");
       let witness = await circuit.calculateWitness(input);
       await circuit.checkConstraints(witness);
+      console.log("Proof Not Verified!");
       assert(false);
     } catch (e) {
       assert.equal(e.message.slice(0, 21), "Error: Assert Failed.");
       assert.equal(
         e.message.slice(22, 59),
-        "Error in template PytPos_154 line: 65"
+        "Error in template proofOfSolvency16 line: 65"
       );
     }
   });
-
-  it("shouldn't verify a proof of inclusion of a non-existing entry", async () => {
-    // pack into circom input adding assetsSum property - assetsSum > liabilitiesSum
+  it("Should NOT verify a proof of inclusion of an entry if the entry is NON-EXISTENT", async () => {
+    console.log("Packing the inputs into the circom zkSNARK construction...");
     const input = createCircomInput(proof, BigInt(3273939305));
-
-    // add a non-existing entry to the input
+    console.log("Adding a fake user intentionally!");
     input.username = BigInt(123456789);
-
     try {
+      console.log("Computing the leaf hashes of the given user data");
+      console.log("Calculating the witness....");
       let witness = await circuit.calculateWitness(input);
       await circuit.checkConstraints(witness);
+      console.log("Proof Not Verified!");
       assert(false);
     } catch (e) {
       assert.equal(e.message.slice(0, 21), "Error: Assert Failed.");
       assert.equal(
         e.message.slice(22, 59),
-        "Error in template PytPos_154 line: 59"
+        "Error in template proofOfSolvency16 line: 59"
       );
     }
   });
-
-  it("shouldn't verify a proof of inclusion based on an invalid root", async () => {
-    // pack into circom input adding assetsSum property - assetsSum > liabilitiesSum
+  it("Should NOT verify a proof of inclusion based on the inclusion of an INVALID root", async () => {
+    console.log("Packing the inputs into the circom zkSNARK construction...");
     const input = createCircomInput(proof, BigInt(3273939305));
-
-    // Invalidate the root
+    console.log("Adding an invalid root intentionally!");
     input.rootHash = input.rootHash + 1n;
-
     try {
+      console.log("Computing the leaf hashes of the given user data");
+      console.log("Calculating the witness....");
       let witness = await circuit.calculateWitness(input);
       await circuit.checkConstraints(witness);
+      console.log("Proof Not Verified!");
       assert(false);
     } catch (e) {
       assert.equal(e.message.slice(0, 21), "Error: Assert Failed.");
       assert.equal(
         e.message.slice(22, 59),
-        "Error in template PytPos_154 line: 59"
+        "Error in template proofOfSolvency16 line: 59"
       );
     }
   });
-
-  it("should generate an error if one of the balances overflows 2**252", async () => {
-    // pack into circom input adding assetsSum property - assetsSum > liabilitiesSum
+  it("Should generate an ERROR if one of the balances overflows 2**252", async () => {
+    console.log("Packing the inputs into the circom zkSNARK construction...");
     const input = createCircomInput(proof, BigInt(3273939305));
-
-    // add a balance that overflows 2**252 to the input
+    console.log("Adding a balance that overflows 2**252 to the input!");
     input.balance = exports.p - 1n;
-
     try {
+      console.log("Computing the leaf hashes of the given user data");
+      console.log("Calculating the witness....");
       let witness = await circuit.calculateWitness(input);
       await circuit.checkConstraints(witness);
+      console.log("Proof Not Verified!");
       assert(false);
     } catch (e) {
       assert.equal(e.message.slice(0, 21), "Error: Assert Failed.");
       assert.equal(
-        e.message.slice(61, 99),
-        "Error in template SafeSum_152 line: 33"
+        e.message.slice(22, 59),
+        "Error in template SafeCalc line: 33"
       );
     }
   });
-
-  it("should generate an error if the sum of two balances overflows 2**252", async () => {
-    // pack into circom input adding assetsSum property - assetsSum > liabilitiesSum
+  it("Should generate an ERROR if sum of 2 balances overflows 2**252", async () => {
+    console.log("Packing the inputs into the circom zkSNARK construction...");
     const input = createCircomInput(proof, BigInt(3273939305));
-
-    // add a balance that will overflow 2**252 when added to the another balance
+    console.log(
+      "Adding a balance that will overflows 2**252 when added to another balance!"
+    );
     input.balance = BigInt(2 ** 252) - 1n;
-
     try {
+      console.log("Computing the leaf hashes of the given user data");
+      console.log("Calculating the witness....");
       let witness = await circuit.calculateWitness(input);
       await circuit.checkConstraints(witness);
+      console.log("Proof Not Verified!");
       assert(false);
     } catch (e) {
       assert.equal(e.message.slice(0, 21), "Error: Assert Failed.");
       assert.equal(
-        e.message.slice(61, 99),
-        "Error in template SafeSum_152 line: 33"
+        e.message.slice(22, 59),
+        "Error in template SafeCalc line: 33"
       );
     }
   });
