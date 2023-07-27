5d8bb49be5a5e9c87678d09b685947eaabc3c221
Agnish Ghosh <iamagnix@gmail.com>
2023-07-27 12:53:56 +0530

    chore: added types for CircomInput, Proof, FullProof, SnarkProverArtifacts

---
+++ zkVerifierServer/src/prover.ts
@@ -0,0 +1,1 @@
+import { SnarkProverArtifacts, FullProof, CircomInput } from "./types";
---
+++ zkVerifierServer/src/types/index.ts
@@ -0,0 +1,31 @@
+import { Entry } from "@0xagnish/zk-data-prep";
+
+export type CircomInput = {
+  rootHash: bigint;
+  username: bigint;
+  balance: bigint;
+  pathIndices: number[];
+  siblingsHashes: bigint[];
+  siblingSums: bigint[];
+  assetsSums: bigint;
+};
+
+export type Proof = {
+  pi_a: bigint[];
+  pi_b: bigint[][];
+  pi_c: bigint[];
+  protocol: string;
+  curve: string;
+};
+
+export type SnarkProverArtifacts = {
+  wasmFilePath: string;
+  zKeyFilePath: string;
+};
+
+export type FullProof = {
+  entry: Entry;
+  rootHash: bigint;
+  assetsSum: bigint;
+  proof: Proof;
+};
